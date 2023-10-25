27ecc5292b1bd48f14adadf911173dbfe71fc36d
JohnH <John.Heckerman@diabolicdesigns.com>
2023-10-24 23:57:16 -0500

    Updates for 2.2.2

--- lotro_importer.php
+++ lotro_importer.php
@@ -1,5 +1,5 @@
 <?php
-//Version: lotro_importer 2.2.1
+//Version: lotro_importer 2.2.2
 // Enable error reporting and display errors for debugging
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -45,14 +45,6 @@ if ($db->connect_errno) {
     die('<span style="color: red;">' . $errorMessage . '</span>');
 }
 
-// Path to your XML file (assuming it's in the same directory)
-$xml_file = 'lotro_import/roster.xml'; // Adjust the path to your XML file
-
-// Load the XML file
-$xml = simplexml_load_file($xml_file);
-
-$updates_made = false; // Flag to track if any updates were made
-
 // Mapping of class names to numeric values
 $class_mapping = [
     'Minstrel' => 1,
@@ -82,187 +74,155 @@ $race_mapping = [
     'Stoutaxedwarf' => 7,
 ];
 
-// Get the list of character names from the XML file
-$characterNamesInXML = [];
+// Load the XML file
+$xml = simplexml_load_file($xml_file);
 
-foreach ($xml->member as $member) {
-    $characterNamesInXML[] = (string)$member['name'];
-}
+if ($xml !== false) {
+    // XML file loaded successfully, continue processing
 
-// Fetch all character names from the database
-$characterNamesInDB = [];
-$query = "SELECT member_name FROM {$table_prefix}members";
-$result = $db->query($query);
-if ($result) {
-    while ($row = $result->fetch_assoc()) {
-        $characterNamesInDB[] = $row['member_name'];
-    }
-}
+    $updates_made = false; // Flag to track if any updates were made
 
-// Compare character names in the database with the XML
-$charactersToRemove = array_diff($characterNamesInDB, $characterNamesInXML);
+    // Process the member data
+    foreach ($xml->member as $member) {
+        $name = (string)$member['name'];
+        $class_name = ucwords(strtolower((string)$member['class']));
+        $race_name = ucwords(strtolower((string)$member['race']));
+        $level = (int)$member['level'];
+        $rank = (int)$member['rank'];
 
-// Remove characters that are not in the XML
-if (!empty($charactersToRemove)) {
-    $charactersToRemoveList = implode("', '", $charactersToRemove);
-    $deleteQuery = "DELETE FROM {$table_prefix}members WHERE member_name IN ('$charactersToRemoveList')";
-    if ($db->query($deleteQuery)) {
-        echo '<span style="color: green;">Characters not in XML file removed from the database.</span><br>';
-    } else {
-        $errorMessage = 'Error removing characters not in XML file: ' . $db->error;
-        logError($errorMessage); // Log the error
-        echo '<span style="color: red; font-style: italic;">' . $errorMessage . '</span><br>';
-    }
+        // Check if the character already exists in the database
+        $query = "SELECT member_name, profiledata, member_rank_id FROM {$table_prefix}members WHERE member_name = ?";
+        $stmt = $db->prepare($query);
 
-    // Echo the removed character names in red italic
-    if (!empty($charactersToRemove)) {
-        echo '<span style="color: red; font-style: italic;">Characters removed from the database: ' . implode(', ', $charactersToRemove) . '</span><br>';
-    }
-}
-
-// Process the member data
-foreach ($xml->member as $member) {
-    $name = (string)$member['name'];
-    $class_name = strtolower((string)$member['class']);
-    $race_name = strtolower((string)$member['race']);
-    $level = (int)$member['level'];
-    $rank = (int)$member['rank'];
-
-    // Convert class and race names to numeric values
-    $class = isset($class_mapping[$class_name]) ? $class_mapping[$class_name] : null;
-    $race = isset($race_mapping[$race_name]) ? $race_mapping[$race_name] : null;
-
-    // Build the JSON data for 'profiledata' column
-    $profiledata = json_encode([
-        'faction' => '',
-        'race' => $race !== null ? (string) $race : '', // Ensure race is not null
-        'class' => $class !== null ? (string) $class : '', // Ensure class is not null
-        'level' => (int) $level,
-        'profession1' => 'farmer',
-        'profession1_mastery' => 0,
-        'profession1_proficiency' => 0,
-        'profession2' => 'farmer',
-        'profession2_mastery' => 0,
-        'profession2_proficiency' => 0,
-        'profession3' => 'farmer',
-        'profession3_mastery' => 0,
-        'profession3_proficiency' => 0,
-        'vocation' => 'armourer', // Corrected vocation value
-    ]);
-
-    // Check if the character already exists in the database
-    $query = "SELECT member_id, profiledata, last_update FROM {$table_prefix}members WHERE member_name = ?";
-    $stmt = $db->prepare($query);
-
-    if (!$stmt) {
-        $errorMessage = 'Error preparing SELECT statement for ' . $name . ': ' . $db->error;
-        logError($errorMessage); // Log the error
-        echo '<span style="color: red;">' . $errorMessage . '</span><br>';
-        continue; // Skip to the next character
-    }
-
-    $stmt->bind_param("s", $name);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        // Character exists, update the 'profiledata', 'member_rank_id', and 'last_update' columns
-        $row = $result->fetch_assoc();
-        $member_id = $row['member_id'];
-        $existingProfileData = json_decode($row['profiledata'], true);
-        $lastUpdateTimestamp = $row['last_update'];
-
-        // Check if any changes need to be made
-        $needsUpdate = false;
-
-        if ($existingProfileData['level'] !== $level) {
-            // Level has changed, update it
-            $existingProfileData['level'] = $level;
-            $needsUpdate = true;
-        }
-
-        // Merge the existing profiledata with the new level information
-        $updatedProfileData = array_merge($existingProfileData, [
-            'level' => $level
-        ]);
-
-        // Check if any updates are needed
-        if ($needsUpdate) {
-            $update_query = "UPDATE {$table_prefix}members SET profiledata = ?, member_rank_id = ?, last_update = ? WHERE member_id = ?";
-            $update_stmt = $db->prepare($update_query);
-
-            if (!$update_stmt) {
-                $errorMessage = 'Error preparing UPDATE statement for ' . $name . ': ' . $db->error;
-                logError($errorMessage); // Log the error
-                echo '<span style="color: red;">' . $errorMessage . '</span><br>';
-                continue; // Skip to the next character
-            }
-
-            $currentTimestamp = time();
-
-            $updatedProfileData_json = json_encode($updatedProfileData);
-
-            $update_stmt->bind_param("siii", $updatedProfileData_json, $rank, $currentTimestamp, $member_id);
-
-            if ($update_stmt->execute()) {
-                echo '<span style="color: green;">Character data for ' . $name . ' updated successfully.</span><br>';
-                $updates_made = true;
-            } else {
-                $errorMessage = 'Error updating character data for ' . $name . ': ' . $update_stmt->error;
-                logError($errorMessage); // Log the error
-                echo '<span style="color: red;">' . $errorMessage . '</span><br>';
-            }
-
-            $update_stmt->close();
-        }
-    } else {
-        // Character does not exist, insert a new record
-        $insert_query = "INSERT INTO {$table_prefix}members (member_name, profiledata, member_rank_id, last_update) VALUES (?, ?, ?, ?)";
-        $insert_stmt = $db->prepare($insert_query);
-
-        if (!$insert_stmt) {
-            $errorMessage = 'Error preparing INSERT statement for ' . $name . ': ' . $db->error;
+        if (!$stmt) {
+            $errorMessage = 'Error preparing SELECT statement for ' . $name . ': ' . $db->error;
             logError($errorMessage); // Log the error
             echo '<span style="color: red;">' . $errorMessage . '</span><br>';
             continue; // Skip to the next character
         }
 
-        $currentTimestamp = time();
+        $stmt->bind_param("s", $name);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        $insert_stmt->bind_param("ssii", $name, $profiledata, $rank, $currentTimestamp);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $existingProfileData = json_decode($row['profiledata'], true);
+            $existingMemberRankId = (int)$row['member_rank_id'];
 
-        $name_param = $name;
-        $profiledata_param = $profiledata;
-        $rank_param = $rank;
-        $currentTimestamp_param = $currentTimestamp;
+            // Check if any changes need to be made
+            $needsUpdate = false;
 
-        $insert_stmt->bind_param("ssii", $name_param, $profiledata_param, $rank_param, $currentTimestamp_param);
+            if ($existingProfileData['level'] !== $level) {
+                $existingProfileData['level'] = $level;
+                $needsUpdate = true;
+            }
 
-        if ($insert_stmt->execute()) {
-            echo '<span style="color: green;">New character data for ' . $name . ' inserted successfully.</span><br>';
-            $updates_made = true;
+            // Cross-reference class and race names with numeric values from mappings
+            $class = isset($class_mapping[$class_name]) ? $class_mapping[$class_name] : $existingProfileData['class'];
+            $race = isset($race_mapping[$race_name]) ? $race_mapping[$race_name] : $existingProfileData['race'];
+
+            if ($existingProfileData['class'] !== $class) {
+                $existingProfileData['class'] = $class;
+                $needsUpdate = true;
+            }
+
+            if ($existingProfileData['race'] !== $race) {
+                $existingProfileData['race'] = $race;
+                $needsUpdate = true;
+            }
+
+            // Check for changes in rank
+            if ($existingMemberRankId !== $rank) {
+                $existingMemberRankId = $rank; // Update member_rank_id
+                $needsUpdate = true;
+            }
+
+            if ($needsUpdate) {
+                $update_query = "UPDATE {$table_prefix}members SET profiledata = ?, member_rank_id = ? WHERE member_name = ?";
+                $update_stmt = $db->prepare($update_query);
+
+                if ($update_stmt) {
+                    $updatedProfileData_json = json_encode($existingProfileData);
+
+                    $update_stmt->bind_param("sis", $updatedProfileData_json, $existingMemberRankId, $name);
+
+                    if ($update_stmt->execute()) {
+                        echo '<span style="color: green;">Character data for ' . $name . ' updated successfully.</span><br>';
+                        $updates_made = true;
+                    } else {
+                        $errorMessage = 'Error updating character data for ' . $name . ': ' . $update_stmt->error;
+                        logError($errorMessage); // Log the error
+                        echo '<span style="color: red;">' . $errorMessage . '</span><br>';
+                    }
+
+                    $update_stmt->close();
+                } else {
+                    $errorMessage = 'Error preparing UPDATE statement for ' . $name . ': ' . $db->error;
+                    logError($errorMessage); // Log the error
+                    echo '<span style="color: red;">' . $errorMessage . '</span><br>';
+                }
+            }
         } else {
-            $errorMessage = 'Error inserting character data for ' . $name . ': ' . $insert_stmt->error;
-            logError($errorMessage); // Log the error
-            echo '<span style="color: red;">' . $errorMessage . '</span><br>';
+            // Character does not exist, insert a new record
+            $profiledata = json_encode([
+                'faction' => '',
+                'race' => $race,
+                'class' => $class,
+                'level' => $level,
+                'profession1' => 'farmer',
+                'profession1_mastery' => 0,
+                'profession1_proficiency' => 0,
+                'profession2' => 'farmer',
+                'profession2_mastery' => 0,
+                'profession2_proficiency' => 0,
+                'profession3' => 'farmer',
+                'profession3_mastery' => 0,
+                'profession3_proficiency' => 0,
+                'vocation' => 'armourer',
+            ]);
+
+            $insert_query = "INSERT INTO {$table_prefix}members (member_name, profiledata, member_rank_id) VALUES (?, ?, ?)";
+            $insert_stmt = $db->prepare($insert_query);
+
+            if ($insert_stmt) {
+                $insert_stmt->bind_param("sis", $name, $profiledata, $rank);
+
+                if ($insert_stmt->execute()) {
+                    echo '<span style="color: green;">New character data for ' . $name . ' inserted successfully.</span><br>';
+                    $updates_made = true;
+                } else {
+                    $errorMessage = 'Error inserting character data for ' . $name . ': ' . $insert_stmt->error;
+                    logError($errorMessage); // Log the error
+                    echo '<span style="color: red;">' . $errorMessage . '</span><br>';
+                }
+
+                $insert_stmt->close();
+            } else {
+                $errorMessage = 'Error preparing INSERT statement for ' . $name . ': ' . $db->error;
+                logError($errorMessage); // Log the error
+                echo '<span style="color: red;">' . $errorMessage . '</span><br>';
+            }
         }
-
-        $insert_stmt->close();
     }
-}
 
-if ($updates_made) {
-    echo '<span style="color: green;"><b>Character data updated/inserted successfully.</b></span><br>';
-} else {
-    echo '<span style="color: blue;"><b>No updates/inserts were made.</b></span><br>';
-}
+    if ($updates_made) {
+        echo '<span style="color: green;"><b>Character data updated/inserted successfully.</b></span><br>';
+    } else {
+        echo '<span style="color: blue;"><b>No updates/inserts were made.</b></span><br>';
+    }
 
-// Rename the XML file to indicate it has been processed
-$new_xml_filename = 'lotro_import/roster-' . date('Ymd') . '.xml'; // Rename with the current date
-if (rename($xml_file, $new_xml_filename)) {
-    echo '<span style="color: green;">File renamed successfully to ' . $new_xml_filename . '.</span><br>';
+    // Rename the XML file to indicate it has been processed
+    $new_xml_filename = 'lotro_import/roster-' . date('Ymd') . '.xml'; // Rename with the current date
+    if (rename($xml_file, $new_xml_filename)) {
+        echo '<span style="color: green;">File renamed successfully to ' . $new_xml_filename . '.</span><br>';
+    } else {
+        echo '<span style="color: red;">Error renaming the XML file.</span><br>';
+    }
 } else {
-    echo '<span style="color: red;">Error renaming the XML file.</span><br>';
+    $errorMessage = 'Error loading XML file: ' . libxml_get_last_error();
+    logError($errorMessage); // Log the error
+    die('<span style="color: red;">' . $errorMessage . '</span>');
 }
 
 // Close the database connection
